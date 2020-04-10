67a6ca2cab7ba97d91be229d7197207c57e0fafa
Nino Chimento <saninoc84@gmail.com>
2020-04-10 12:23:19 +0200

    start

---
+++ database/migrations/2020_04_10_100201_create_flats_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFlatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flats', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+            $table->string("title",250);
+            $table->string("img")->nullable();
+            $table->string("address")->nullable();
+            $table->string("position")->nullable();
+            $table->string("slug")->unique();
+            $table->text("summary")->nullable();
+            $table->integer("rooms")->unsigned();
+            $table->integer("bathrooms")->unsigned();
+            $table->integer("mq")->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flats');
+    }
+}
